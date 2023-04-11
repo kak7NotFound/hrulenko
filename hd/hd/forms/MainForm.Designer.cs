038259550ab615789f088b7df5f7525f4f0cf800
kak7 <38856979+kak7NotFound@users.noreply.github.com>
2023-04-11 15:01:21 +0300

    a

---
+++ hd/hd/forms/MainForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿using System.ComponentModel;
+
+namespace hd.forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.q = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.result = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(34, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя: Иван Иванов";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(34, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Пройдено: 3% (3/100)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(323, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Выход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(486, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { this.title, this.q, this.result });
+            this.dataGridView1.Location = new System.Drawing.Point(22, 109);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(498, 204);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // title
+            // 
+            this.title.HeaderText = "Название";
+            this.title.Name = "title";
+            this.title.ReadOnly = true;
+            this.title.Width = 150;
+            // 
+            // q
+            // 
+            this.q.HeaderText = "Вопросов";
+            this.q.Name = "q";
+            this.q.ReadOnly = true;
+            this.q.Width = 60;
+            // 
+            // result
+            // 
+            this.result.HeaderText = "Результат";
+            this.result.Name = "result";
+            this.result.ReadOnly = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(92, 333);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 41);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Пройти тест";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 420);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Главная";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button3;
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn q;
+        private System.Windows.Forms.DataGridViewTextBoxColumn result;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ hd/hd/forms/MainForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace hd.forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            dataGridView1.Rows.Add("Пожарная безопасность","20","Не пройдено");
+            dataGridView1.Rows.Add("Станок ММУ13-Л3","80","Не пройдено");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
